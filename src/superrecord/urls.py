8413d6c58d0cc30b28bf15ffba9eb2cd0b3118f4
anyric <anyamaronyango@gmail.com>
2019-07-11 18:43:04 +0300

    feature(settings-page): add admin settings page
    - add settings, user, sales and expense templates
    - add settings, user, sales and expense views
    - add settings, user, sales and expense urls
    - add css style and js scripts
    
    [Finishes #167203654]

--- src/superrecord/urls.py
+++ src/superrecord/urls.py
@@ -8,10 +8,14 @@ from accounts import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('accounts/user/', user_views.user, name='user'),
     path('accounts/home/', user_views.home, name='home'),
     path('accounts/signup/', user_views.register, name='signup'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
     path('accounts/product/', user_views.product, name='product'),
     path('accounts/purchase/', user_views.purchase, name='purchase'),
+    path('accounts/sales/', user_views.sales, name='sales'),
+    path('accounts/expense/', user_views.expense, name='expense'),
+    path('accounts/setting/', user_views.setting, name='setting'),
 ]
