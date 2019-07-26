cb0682e10bdee891952a517d72975f42ed9dc7a7
anyric <anyamaronyango@gmail.com>
2019-07-26 21:56:38 +0300

    chore(tests): add tests for accounts app
    - add test for user and roles models
    - add test for permission functions
    
    [Maintains #167545360]

---
+++ src/accounts/tests/test_models.py
@@ -0,0 +1,121 @@
+from django.test import TestCase
+from ..models import User, Role
+
+class UserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user("test", "test@info.com", "1234@test")
+        User.objects.create_superuser("admintest", "admintest@info.com", "1234@admin")
+
+    def test_regular_user_was_created(self):
+        test = User.objects.get(username="test")
+       
+        self.assertEqual(test.username, "test")
+        self.assertEqual(test.email, "test@info.com")
+
+    def test_admin_user_was_created(self):
+        admin = User.objects.get(username="admintest")
+
+        self.assertEqual(admin.username, "admintest")
+        self.assertEqual(admin.email, "admintest@info.com")
+
+    def test_regular_user_details_not_equal(self):
+        test = User.objects.get(username="test")
+       
+        self.assertNotEqual(test.username, "tests")
+        self.assertNotEqual(test.email, "tests@info.com")
+
+    def test_admin_user_details_not_equal(self):
+        admin = User.objects.get(username="admintest")
+
+        self.assertNotEqual(admin.username, "admintests")
+        self.assertNotEqual(admin.email, "admintests@info.com")
+
+    def test_username_label(self):
+        test = User.objects.get(id=1)
+        field_label = test._meta.get_field('username').verbose_name
+        self.assertEquals(field_label, 'username')
+    
+    def test_username_max_length(self):
+        test = User.objects.get(id=1)
+        max_length = test._meta.get_field('username').max_length
+        self.assertEquals(max_length, 255)
+
+    def test_email_label(self):
+        test = User.objects.get(id=2)
+        field_label = test._meta.get_field('email').verbose_name
+        self.assertEquals(field_label, 'email address')
+
+    def test_email_max_length(self):
+        test = User.objects.get(id=1)
+        max_length = test._meta.get_field('email').max_length
+        self.assertEquals(max_length, 255)
+
+    def test_password_label(self):
+        test = User.objects.get(id=2)
+        field_label = test._meta.get_field('password').verbose_name
+        self.assertEquals(field_label, 'password')
+
+    def test_is_admin_label(self):
+        test = User.objects.get(id=2)
+        field_label = test._meta.get_field('is_admin').verbose_name
+        self.assertEquals(field_label, 'is admin')
+
+    def test_is_active_label(self):
+        test = User.objects.get(id=2)
+        field_label = test._meta.get_field('is_active').verbose_name
+        self.assertEquals(field_label, 'is active')
+    
+    def test_created_at_label(self):
+        test = User.objects.get(id=2)
+        field_label = test._meta.get_field('created_at').verbose_name
+        self.assertEquals(field_label, 'created at')
+    
+    def test_updated_at_label(self):
+        test = User.objects.get(id=2)
+        field_label = test._meta.get_field('updated_at').verbose_name
+        self.assertEquals(field_label, 'updated at')
+
+    def test_last_login_label(self):
+        test = User.objects.get(id=2)
+        field_label = test._meta.get_field('last_login').verbose_name
+        self.assertEquals(field_label, 'last login')
+
+    def test_user_count(self):
+        count = User.objects.count()
+        self.assertEqual(count, 2)
+
+class RoleModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Role.objects.create(name="test", description="This is a test role")
+        Role.objects.create(name="admin", description="This is a admin role")
+
+    def test_role_was_created(self):
+        test = Role.objects.get(name="test")
+        admin = Role.objects.get(name="admin")
+       
+        self.assertEqual(test.name, "test")
+        self.assertEqual(test.description, "This is a test role")
+
+        self.assertEqual(admin.name, "admin")
+        self.assertEqual(admin.description, "This is a admin role")
+
+    def test_role_name_label(self):
+        test = Role.objects.get(id=1)
+        field_label = test._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+    
+    def test_role_description_label(self):
+        test = Role.objects.get(id=1)
+        field_label = test._meta.get_field('description').verbose_name
+        self.assertEquals(field_label, 'description')
+    
+    def test_role_description_max_length(self):
+        test = Role.objects.get(id=1)
+        max_length = test._meta.get_field('description').max_length
+        self.assertEquals(max_length, 100)
+
+    def test_role_count(self):
+        count = Role.objects.count()
+        self.assertEqual(count, 2)
