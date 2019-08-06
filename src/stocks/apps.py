fe783bb2532353693084ebb4908583b9f2447ff4
anyric <anyamaronyango@gmail.com>
2019-08-06 16:12:43 +0300

    feature(product-model): add model for the product app
    - add model class
    - create migrations
    - add tests for the model
    
    [Finishes #167701789]

--- src/stocks/apps.py
+++ src/stocks/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class ProductsConfig(AppConfig):
+class StocksConfig(AppConfig):
     name = 'stocks'
--- src/superrecord/settings.py
+++ src/superrecord/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'stocks.apps.ProductsConfig',
+    'stocks.apps.StocksConfig',
     'crispy_forms',
     'bootstrap_modal_forms',
     'widget_tweaks',
