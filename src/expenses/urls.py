6755d95fcd93f01ed91a09dbc895a8b68ffcc49f
anyric <anyamaronyango@gmail.com>
2019-09-13 12:49:02 +0300

    feature(expenses-views): add expenses views
    - add expense category view classes
    - add expense category view templates
    - add expense category forms
    - add expense view classes
    - add expense view templates
    - add expense forms
    - add expense category tests
    - add expense tests
    - add expense urls
    
    [Finishes #168433335]

--- src/expenses/urls.py
+++ src/expenses/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from expenses import views as expenses_views
 
 urlpatterns = [
+    path('categories/', expenses_views.CategoryListView.as_view(), name='categories'),
+    path('category/', expenses_views.CategoryCreationView.as_view(), name='category'),
+    path('edit_category/<int:id>/', expenses_views.EditCategoryView.as_view(), name='edit_category'),
+    path('delete_category/<int:id>/', expenses_views.DeleteCategoryView.as_view(), name='delete_category'),
     path('expenses/', expenses_views.ExpensesListView.as_view(), name='expenses'),
-    # path('expense/', expenses_views.ExpensesCreationView.as_view(), name='expense'),
-    # path('edit_expense/<int:id>/', expenses_views.EditExpensesView.as_view(), name='edit_expense'),
-    # path('delete_expense/<int:id>/', expenses_views.DeleteExpensesView.as_view(), name='delete_expense')
+    path('expense/', expenses_views.ExpenseCreationView.as_view(), name='expense'),
+    path('edit_expense/<int:id>/', expenses_views.EditExpenseView.as_view(), name='edit_expense'),
+    path('delete_expense/<int:id>/', expenses_views.DeleteExpenseView.as_view(), name='delete_expense')
 ]
--- src/expenses/views.py
+++ src/expenses/views.py
@@ -1,13 +1,111 @@
-# from django.http import HttpResponseRedirect
-# from django.shortcuts import render
-# from django.db.models import Sum
+from django.http import HttpResponseRedirect
+from django.db.models import Sum
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-# from django.urls import reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic import (
-    # ListView, UpdateView, DetailView, DeleteView, CreateView,
-    TemplateView)
+    ListView, UpdateView, DetailView, DeleteView, CreateView)
+from decorators.decorators import group_required
+from expenses.models import ExpenseCategory, Expenses
+from expenses.forms import (
+    EditCategoryForm, EditExpenseForm,
+    CategoryCreationForm, ExpenseCreationForm)
+
+decorators = [group_required(['Admin','Manager','General Manager'])]
+@method_decorator(login_required, name="dispatch")
+class CategoryListView(ListView):
+    queryset = ExpenseCategory.objects.all().order_by('-id')
+    paginate_by = 10
+    context_object_name = 'categories'
+    template_name = 'expenses/expenses_category.html'
+
+@method_decorator(decorators, name='dispatch')
+class CategoryCreationView(CreateView):
+    form_class = CategoryCreationForm
+    template_name = 'expenses/add_category.html'
+    success_message = 'Success: Category creation succeeded.'
+    success_url = reverse_lazy('setting')
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            'name': request.POST.get('name', None),
+            'description': request.POST.get('description', None),
+            'created_by': request.user.id
+        }
+        if request.method == 'POST':
+            form = self.form_class(data)
+
+            if form.is_valid():
+                form.save()
+
+                return HttpResponseRedirect(self.success_url)
+
+        return super().post(request, *args, **kwargs)
+
+@method_decorator(decorators, name='dispatch')
+class EditCategoryView(UpdateView, DetailView):
+    template_name = 'expenses/edit_category.html'
+    pk_url_kwarg = 'id'
+    form_class = EditCategoryForm
+    queryset = ExpenseCategory.objects.all()
+    success_url = reverse_lazy('setting')
+
+@method_decorator(decorators, name='dispatch')
+class DeleteCategoryView(DeleteView):
+    template_name = 'expenses/delete_category.html'
+    pk_url_kwarg = 'id'
+    queryset = ExpenseCategory.objects.all()
+    success_url = reverse_lazy('setting')
 
 @method_decorator(login_required, name="dispatch")
-class ExpensesListView(TemplateView):
+class ExpensesListView(ListView):
+    queryset = Expenses.objects.all().order_by('-id')
+    paginate_by = 10
+    context_object_name = 'expenses'
     template_name = 'expenses/expenses.html'
+
+@method_decorator(login_required, name='dispatch')
+class ExpenseCreationView(CreateView):
+    form_class = ExpenseCreationForm
+    template_name = 'expenses/add_expense.html'
+    success_message = 'Success: Expense creation succeeded.'
+    success_url = reverse_lazy('expense')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['expenses'] = Expenses.objects.filter(created_by=self.request.user.id)
+        context['total_expenses'] = Expenses.objects.filter(created_by=self.request.user.id).aggregate(Sum('amount'))
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            'category': int(request.POST.get('category', 0)),
+            'description': request.POST.get('description', None),
+            'amount': request.POST.get('amount', 0),
+            'created_by': request.user.id
+        }
+        if request.method == 'POST':
+            form = self.form_class(data)
+
+            if form.is_valid():
+                form.save()
+
+                return HttpResponseRedirect(self.success_url)
+
+        return super().post(request, *args, **kwargs)
+
+@method_decorator(decorators, name='dispatch')
+class EditExpenseView(UpdateView, DetailView):
+    template_name = 'expenses/edit_expense.html'
+    pk_url_kwarg = 'id'
+    form_class = EditExpenseForm
+    queryset = Expenses.objects.all()
+    success_url = reverse_lazy('expense')
+
+@method_decorator(decorators, name='dispatch')
+class DeleteExpenseView(DeleteView):
+    template_name = 'expenses/delete_expense.html'
+    pk_url_kwarg = 'id'
+    queryset = Expenses.objects.all()
+    success_url = reverse_lazy('expense')
